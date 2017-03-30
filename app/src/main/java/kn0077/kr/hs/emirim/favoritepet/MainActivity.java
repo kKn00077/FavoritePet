fb301eed7ab36bdc9406e3836a41718e71713f45
kKn00077 <kkn00077@gmail.com>
2017-03-30 20:57:57 +0900

    #4 button event

--- app/src/main/java/kn0077/kr/hs/emirim/favoritepet/MainActivity.java
+++ app/src/main/java/kn0077/kr/hs/emirim/favoritepet/MainActivity.java
@@ -10,8 +10,9 @@ import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener {
+public class MainActivity extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener, View.OnClickListener {
 
     CheckBox checkSelect; /*레이아웃 위젯을 자바(필드)로 불러오기 위해 참조변수 먼저 선언. 아직 값은 NULL값임*/
     TextView textQuest;
@@ -33,11 +34,8 @@ public class MainActivity extends AppCompatActivity implements CompoundButton.On
         butok=(Button) findViewById(R.id.but_ok);
         imgvPet=(ImageView) findViewById(R.id.imgv_pet);
 
-
         checkSelect.setOnCheckedChangeListener(this);
-
-
-
+        butok.setOnClickListener(this);
 
     }
 
@@ -54,6 +52,18 @@ public class MainActivity extends AppCompatActivity implements CompoundButton.On
             rg.setVisibility(View.INVISIBLE);
             butok.setVisibility(View.INVISIBLE);
             imgvPet.setVisibility(View.INVISIBLE);
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (rg.getCheckedRadioButtonId())//라디오 그룹 안에 있는 라디오 버튼 중에서 체크된 라디오 버튼의 아이디를 반환해줌
+        {
+            case R.id.radio_dog : imgvPet.setImageResource(R.drawable.dog); break;
+            case R.id.radio_cat : imgvPet.setImageResource(R.drawable.cat); break;
+            case R.id.radio_rabbit : imgvPet.setImageResource(R.drawable.rabbit); break;
+            default:
+                Toast.makeText(this, "라디오 버튼이 선택이 안되었군요.",Toast.LENGTH_SHORT).show(); //show()가 없으면 토스트가 안보임
         }
     }
 }
